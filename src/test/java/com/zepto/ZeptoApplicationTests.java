1b1a919c231e7c2a6e25914dc2b8d8c7a35ec051
vickykumar1996 <vk7343270@gmail.com>
2024-10-20 15:22:13 +0530

    First commit

---
+++ src/test/java/com/zepto/ZeptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zepto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
