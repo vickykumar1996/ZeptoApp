dc06d6c921c6d22781bbf7c3a6896759d1d22f52
Sangam Maurya <sangammaurya648@gmail.com>
2024-10-21 17:56:46 +0530

    create findAll data inside controller

--- src/main/java/com/zepto/controller/GroceryController.java
+++ src/main/java/com/zepto/controller/GroceryController.java
@@ -4,10 +4,14 @@ import com.zepto.payload.GroceryDto;
 import com.zepto.service.GroceryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,5 +24,11 @@ public class GroceryController {
         groceryService.saveGrocery()
     }
 
+
+    @GetMapping
+    public ResponseEntity<List<GroceryDto>>getAllData(){
+        List<GroceryDto> groceryDtos = groceryService.ListAllData();
+        return new ResponseEntity<>(groceryDtos , HttpStatus.OK);
+    }
 
 }
