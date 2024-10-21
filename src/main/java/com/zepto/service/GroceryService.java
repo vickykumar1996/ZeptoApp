271dff426d2a1da6acb2911c8c3b0c0909d10489
vickykumar1996 <vk7343270@gmail.com>
2024-10-21 14:30:45 +0530

    add service class

--- src/main/java/com/zepto/service/GroceryService.java
+++ src/main/java/com/zepto/service/GroceryService.java
@@ -1,6 +1,13 @@
 package com.zepto.service;
 
 
-public interface GroceryService {
+import com.zepto.payload.GroceryDto;
 
+import java.util.List;
+
+public interface GroceryService {
+    GroceryDto saveGrocery(GroceryDto groceryDto);
+    public List<GroceryDto>ListAllData();
+    public void deleteReg (long id);
+    public GroceryDto updateReg(long id , GroceryDto dto);
 }
---
+++ src/main/java/com/zepto/service/GroceryServiceImpl.java
@@ -0,0 +1,12 @@
+package com.zepto.service;
+
+import com.zepto.payload.GroceryDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GroceryServiceImpl implements GroceryService{
+    @Override
+    public GroceryDto saveGrocery(GroceryDto groceryDto) {
+        return null;
+    }
+}
