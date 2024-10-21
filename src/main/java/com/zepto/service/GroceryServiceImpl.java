add804675d29c460af1c7f03a9043e50d256a358
Sangam Maurya <sangammaurya648@gmail.com>
2024-10-21 14:36:14 +0530

    some change in service

--- src/main/java/com/zepto/service/GroceryServiceImpl.java
+++ src/main/java/com/zepto/service/GroceryServiceImpl.java
@@ -1,20 +1,24 @@
 package com.zepto.service;
 
 import com.zepto.payload.GroceryDto;
+import com.zepto.repository.GroceryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@RequiredArgsConstructor
 @Service
 public class GroceryServiceImpl implements GroceryService{
+    private  final GroceryRepository repository;
     @Override
+
     public GroceryDto saveGrocery(GroceryDto groceryDto) {
         return null;
     }
 
     @Override
     public List<GroceryDto> ListAllData() {
-        return List.of();
+
     }
 
     @Override
